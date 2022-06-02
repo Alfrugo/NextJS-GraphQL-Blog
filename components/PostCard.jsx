14356d43b06e373c9729dbf41829aec0bde1b139
Alfrugo <adiez@mtm-inc.net>
2022-06-02 11:56:00 -0500

    progress

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -7,13 +7,21 @@ const PostCard = ({ post }) => {
     console.log(post)
     return (
         <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 mb-8 pb-12'>
-            <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
+            <div className='relative  shadow-md pb-80 mb-6'>
                 <img 
                     src={post.featuredimage.url}
                     alt={post.title}
                     className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
                     
                     />
+
+            <h1 className='h-20 text-center mb-8 cursor-pointer text-slate-700 text-xl font-semibold'>
+              <Link href={`/post/${post.slug}`}>
+                {post.title}
+              
+              </Link>
+                Hello there 
+            </h1>
 
             </div>
             
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 // import Image from 'next/image'
 import { PostCard, PostWidget, Categories} from '../components'
 
+
 import { getPosts } from '../services'
 
 
@@ -15,7 +16,7 @@ import { getPosts } from '../services'
 // const Home: NextPage = () => {
   export default function Home ({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-100">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>My Next JS GraphQL blog</title>
         <link rel="icon" href="/favicon.ico" />
@@ -27,6 +28,7 @@ import { getPosts } from '../services'
         </div>
         <div className='col-span-1 lg:col-span-4'>
           <div className='lg:sticky relative top-8'>
+            
             <PostWidget />
             <Categories />
           </div>
