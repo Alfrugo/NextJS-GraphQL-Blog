de1369e82be8abe82866d6149e68cc2c64819437
Alfrugo <adiez@mtm-inc.net>
2022-06-04 18:28:20 -0500

    completing post structure

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -6,22 +6,28 @@ const PostCard = ({ post }) => {
 
     console.log(post)
     return (
-        <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 mb-8 pb-12'>
-            <div className='relative  shadow-md pb-80 mb-6'>
+        <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 mb-8 pb-4'>
+            <div className='relative  shadow-md '>
                 <img 
                     src={post.featuredimage.url}
                     alt={post.title}
-                    className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
-                    
-                    />
+                    className='object-top relative h-100 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
+                />
 
-            <h1 className='h-20 text-center mb-8 cursor-pointer text-slate-700 text-xl font-semibold'>
+            <h1 className='pt-5 h-14 text-center  cursor-pointer text-slate-700 text-xl font-semibold'>
               <Link href={`/post/${post.slug}`}>
                 {post.title}
-              
               </Link>
-                Hello there 
             </h1>
+            <div>
+                {moment(post.createdAt).format('MMM DD, YYYY')}
+            </div>
+            <div>
+                {post.author.name}
+            </div>
+            <div className='p-5'>
+                {post.excerpt}
+            </div>
 
             </div>
             
