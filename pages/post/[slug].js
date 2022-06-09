9d94bcebc9fa88ccd356df83d11d88228da99d21
Alfrugo <adiez@mtm-inc.net>
2022-06-09 12:19:47 -0500

    [slug]

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -5,8 +5,24 @@ import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } fr
 
 const PostDetails = () => {
     return (
-        <div>
-            PostDetails
+        <div className='container mx-auto px-10 mb-8'> 
+            <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+                <div className='col-span-1 lg:col-span-8'>
+                    <PostDetail />
+                    <Author />
+                    <CommentsForm />
+                    <Comments />
+                </div>
+                <div className='col-span-1 lg:col-span-4'>
+                    <div className='relative lg:sticky top-8'>
+                        <PostWidget />
+                        <Categories />
+
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     )
 }
