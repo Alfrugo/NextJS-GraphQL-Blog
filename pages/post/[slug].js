c4654ee17df3147becdede145a6526eae9e6b0f1
Alfrugo <adiez@mtm-inc.net>
2022-06-09 11:43:50 -0500

    [slug]

---
+++ pages/post/[slug].js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { getPosts, getPostsDetails } from '../../services'
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
+
+const PostDetails = () => {
+    return (
+        <div>
+            PostDetails
+        </div>
+    )
+}
+
+export default PostDetails
