08316fd852aafb91488fce5f8c03d5dfb846cc08
Alfrugo <adiez@mtm-inc.net>
2022-06-10 11:44:00 -0500

    post details image author and date working

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import moment from 'moment'
 
-const PostDetail = () => {
+const PostDetail = ( { post }) => {
     return (
-        <div>
-            <h1>PostDetail</h1>
+        <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb8'>
+            <div className='relative overflow-hidden shadow-md mb-6'>
+                <img 
+                    src={post.featuredimage.url}
+                    alt={post.title}
+                    className='object-top h-full w-full rounded-t-lg'
+                />
+
+            </div>
+            <div className='px-4 lg:px-0'>
+                <div className=''>
+                <div>
+                {moment(post.createdAt).format('MMM DD, YYYY')}
+            </div>
+            <div>
+                {post.author.name}
+            </div>
+            <h1 className='mb-8 text-3xl font-semibold'>{post.title}</h1>
+
+                </div>
+
+            </div>
         </div>
     )
 }
