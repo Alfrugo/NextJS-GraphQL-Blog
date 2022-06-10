efd3e99c8c91ffd8f50ecc62080eef2bcc7045d1
Alfrugo <adiez@mtm-inc.net>
2022-06-10 15:43:12 -0500

    individual post displays correctly content, image, author, date

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -2,6 +2,45 @@ import React from 'react'
 import moment from 'moment'
 
 const PostDetail = ( { post }) => {
+    const getContentFragment = (index, text, obj, type) => {
+        let modifiedText = text;
+    
+        if (obj) {
+          if (obj.bold) {
+            modifiedText = (<b key={index}>{text}</b>);
+          }
+    
+          if (obj.italic) {
+            modifiedText = (<em key={index}>{text}</em>);
+          }
+    
+          if (obj.underline) {
+            modifiedText = (<u key={index}>{text}</u>);
+          }
+        }
+    
+        switch (type) {
+          case 'heading-three':
+            return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+          case 'paragraph':
+            return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+          case 'heading-four':
+            return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+          case 'image':
+            return (
+              <img
+                key={index}
+                alt={obj.title}
+                height={obj.height}
+                width={obj.width}
+                src={obj.src}
+              />
+            );
+          default:
+            return modifiedText;
+        }
+      };
+
     return (
         <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb8'>
             <div className='relative overflow-hidden shadow-md mb-6'>
@@ -14,16 +53,18 @@ const PostDetail = ( { post }) => {
             </div>
             <div className='px-4 lg:px-0'>
                 <div className=''>
-                <div>
-                {moment(post.createdAt).format('MMM DD, YYYY')}
-            </div>
-            <div>
-                {post.author.name}
-            </div>
-            <h1 className='mb-8 text-3xl font-semibold'>{post.title}</h1>
-
+                    <div>
+                        {moment(post.createdAt).format('MMM DD, YYYY')}
+                    </div>
+                    <div>
+                        {post.author.name}
+                    </div>
                 </div>
-
+                <h1 className='mb-8 text-3xl font-semibold'>{post.title}</h1>
+                {post.content.raw.children.map((typeObj, index)=> {
+                    const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item))
+                    return getContentFragment(index,children, typeObj, typeObj.type)
+                } )}
             </div>
         </div>
     )
