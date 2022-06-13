c6976619d86e844cc982dfb3b2cf86e820d0bf4d
Alfrugo <adiez@mtm-inc.net>
2022-06-13 12:31:30 -0500

    comments errors

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -1,13 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { GraphQLClient, gql } from 'graphql-request';
 
-type Data = {
-  name: string
-}
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+/** *************************************************************
+* Any file inside the folder pages/api is mapped to /api/* and  *
+* will be treated as an API endpoint instead of a page.         *
+*************************************************************** */
+
+// export a default function for API route to work
+export default async function asynchandler(req, res) {
+  const graphQLClient = new GraphQLClient((graphqlAPI), {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) { id }
+    }
+  `;
+
+  const result = await graphQLClient.request(query, {
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment,
+    slug: req.body.slug,
+  });
+
+  return res.status(200).send(result);
 }
