e11f2b51cac680c7326b3e3226242d559646fcda
Alfrugo <adiez@mtm-inc.net>
2022-06-17 12:26:56 -0500

    bugs bugs bugs!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 
-// import type { NextPage } from 'next'
+import type { NextPage } from 'next'
 import Head from 'next/head'
 // import Image from 'next/image'
 import { PostCard, PostWidget, Categories} from '../components'
